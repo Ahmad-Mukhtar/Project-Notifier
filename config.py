babc729c2ca18dcc752870edfc3d7cffbf6fdd28
Ahmad Mukhtar <gracenoteii5@gmail.com>
2025-01-19 11:42:16 +0500

    Created script without chrome driver.

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
-import time
+IDS_TO_CONSIDER = ['996364628025274386', '996364628025274382',
+                   '1031626782284431360','1031626782313791488',
+                   '1031626776085250048','1031626795211276288',
+                   '1225465958237442048'
+                   ]
 
-from win10toast import ToastNotifier
-
-IDS = ['996364628025274386', '1031626738982436864', '1031626732309299200', '996364628025274382', '1031626782284431360',
-       '1031626776085250048', '1031626773660942336', '1031626762999021568', '996364628021080069', '1031626731394940928'
-    , '996364628025274380', '996364628025274381', '1031626741679374336']
-JOB_TYPE = ["Python", "Flask","Django", "Java", 'Spring Boot', 'Saas',
-            'React', 'Nodejs', 'Unity', 'Desktop Application',
-            'C#', 'C++', 'golang']
+JOB_TYPE_TO_CONSIDER = ["Python", "Java", "Spring Boot", "Spring Framework",
+                        "Saas", "Web Development", "Software Development"]
 
 
-def show_notification(title, message, duration):
-    toaster = ToastNotifier()
-    toaster.show_toast(title, message, None, duration)
-    time.sleep(2)
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import os
 import time
 import urllib.request
-
+import cloudscraper
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from pushbullet import Pushbullet
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
-from config import IDS, JOB_TYPE, show_notification
-from scheduler import WAIT_TIME_BEFORE_SCRAPPING, WAIT_TIME_AFTER_SCRAPING
+from config import JOB_TYPE_TO_CONSIDER, IDS_TO_CONSIDER
 
 load_dotenv(".env")
-
 PB_KEY = os.getenv('PB_KEY')
+WAIT_TIME_AFTER_SCRAPING = float(os.getenv("WAIT_TIME_AFTER_SCRAPING"))
+
 pb = Pushbullet(PB_KEY)
 
 
@@ -27,46 +23,43 @@ def is_connected():
     return False
 
 
-service = Service("./chromedriver-win64/chromedriver-win64/chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-
-
-def login():
-    driver.get("https://www.upwork.com/ab/account-security/login")
-    time.sleep(5)
-    username_field = driver.find_element(By.NAME, "login[username]")
-    username_field.send_keys(os.getenv('EMAIL'))
-    username_field.send_keys(Keys.RETURN)
-    time.sleep(5)
-    password_field = driver.find_element(By.NAME, "login[password]")
-    password_field.send_keys(os.getenv('PASS'))
-    password_field.send_keys(Keys.RETURN)
-    time.sleep(5)
-
-
-def getPythonjobs():
+def getJobs():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                      "(HTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
+    }
+    scraper = cloudscraper.create_scraper()
     while True:
         if is_connected():
-            for index, id in enumerate(IDS):
-                driver.get(f"https://www.upwork.com/nx/jobs/search/?ontology_skill_uid={id}&sort=recency")
-                time.sleep(WAIT_TIME_BEFORE_SCRAPPING)
-                element = driver.find_element(By.CSS_SELECTOR, '[data-test="job-tile-list"]')
-                time_ele = element.find_element(By.CSS_SELECTOR, 'span[data-test="UpCRelativeTime"]')
-                print(time_ele.text)
-                posted = time_ele.text.split()
-                posted_time = posted[0] + posted[1]
-                if "minute" in posted_time or "second" in posted_time:
-                    show_notification(title=f"New {JOB_TYPE[index]} Project",
-                                      message="New project Posted about " + posted_time,
-                                      duration=10)
-                    pb.push_note(f"New {JOB_TYPE[index]} Project", "New project Posted about " + posted_time)
-                time.sleep(WAIT_TIME_AFTER_SCRAPING)
-
+            for index, project_id in enumerate(IDS_TO_CONSIDER):
+                response = scraper.get(f"https://www.upwork.com/nx/jobs/search/?ontology_skill_uid={project_id}"
+                                       f"&sort=recency",
+                                       headers=headers)
+                soup = BeautifulSoup(response.content, "html.parser")
+                try:
+                    jobs_section = soup.find("section", {"data-ev-label": "search_result_impression"})
+                    jobs_headers = jobs_section.findAll("div", {"class": "job-tile-header"})
+                    latest_posted_job = jobs_headers[0]
+                    # for job in jobs_headers[:5]:
+                    #     print(job.text)
+                    posted_job_list = latest_posted_job.text.split()
+                    print(f"New {JOB_TYPE_TO_CONSIDER[index]} Project")
+                    print(str(posted_job_list))
+                    posted_time = posted_job_list[1] + " " + posted_job_list[2] + " " + posted_job_list[3]
+                    print(f"Posted Time is : {posted_time}")
+                    if "minute" in posted_time or "second" in posted_time:
+                        pb.push_note(f"New {JOB_TYPE_TO_CONSIDER[index]} Project", latest_posted_job.text)
+                        print("Notification Sent")
+                except Exception as e:
+                    print(f"Some Exception Occurred  {e}")
+                finally:
+                    print("WAIT_TIME_AFTER_SCRAPING")
+                    time.sleep(WAIT_TIME_AFTER_SCRAPING)
+            print("Sleeping for 5 minutes")
+            time.sleep(300)
         else:
             print("Wifi Not Connected")
 
 
-login()
-getPythonjobs()
-
-driver.quit()
+if __name__ == '__main__':
+    getJobs()
