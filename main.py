b61ce9f4cb6b6d455e277a984bf0e17ef7d6fe4f
Ahmad Mukhtar <gracenoteii5@gmail.com>
2025-01-19 14:48:48 +0500

    Removed Extra print

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def getJobs():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9",
         "Connection": "keep-alive",
+        "Accept-Encoding": "gzip, deflate, br",
         "Upgrade-Insecure-Requests": "1"
     }
     scraper = cloudscraper.create_scraper(
