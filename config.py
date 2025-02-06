76afabb81c8a3bb3df6e629524c83d6c305101c3
Ahmad Mukhtar <gracenoteii5@gmail.com>
2025-02-06 15:08:01 +0500

    Added golang and fixed headers.

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 IDS_TO_CONSIDER = ['996364628025274386', '996364628025274382',
-                   '1031626782284431360','1031626782313791488',
-                   '1031626776085250048','1031626795211276288',
+                   '1031626741679374336',
+                   '1031626782284431360', '1031626782313791488',
+                   '1031626776085250048', '1031626795211276288',
                    '1225465958237442048'
                    ]
 
-JOB_TYPE_TO_CONSIDER = ["Python", "Java", "Spring Boot", "Spring Framework",
+JOB_TYPE_TO_CONSIDER = ["Python", "Java", "Golang", "Spring Boot", "Spring Framework",
                         "Saas", "Web Development", "Software Development"]
-
-
--- main.py
+++ main.py
@@ -10,9 +10,8 @@ from config import JOB_TYPE_TO_CONSIDER, IDS_TO_CONSIDER
 load_dotenv(".env")
 PB_KEY = os.getenv('PB_KEY')
 WAIT_TIME_AFTER_SCRAPING = float(os.getenv("WAIT_TIME_AFTER_SCRAPING"))
-
+WAIT_TIME_AFTER_SCRAPING_ALL = float(os.getenv("WAIT_TIME_AFTER_SCRAPING_ALL"))
 pb = Pushbullet(PB_KEY)
-
 
 def is_connected():
     try:
@@ -25,12 +24,14 @@ def is_connected():
 
 def getJobs():
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1"
-    }
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                        "Accept-Language": "en-US,en;q=0.9",
+                            "Connection": "keep-alive",
+                                "Upgrade-Insecure-Requests": "1",
+                                "Referer": "https://www.google.com/",  # Replace with a valid referer if necessary
+                                    "Origin": "https://www.upwork.com"    # Replace with the appropriate origin
+                                        }
     scraper = cloudscraper.create_scraper(
         browser={
             'browser': 'firefox',
@@ -41,21 +42,18 @@ def getJobs():
     while True:
         if is_connected():
             for index, project_id in enumerate(IDS_TO_CONSIDER):
-                response = scraper.get(
-                    f"https://www.upwork.com/nx/jobs/search/?ontology_skill_uid={project_id}&sort=recency",
-                    headers=headers)
+                job_url =f"https://www.upwork.com/nx/jobs/search/?ontology_skill_uid={project_id}&sort=recency"
+                response = scraper.get(job_url,headers=headers)
                 soup = BeautifulSoup(response.content, "html.parser")
                 try:
-                    print(f"Soup is {soup.text}")
                     jobs_section = soup.find("section")
-                    print(f"Jobs_section {jobs_section}")
                     jobs_headers = jobs_section.findAll("div", {"class": "job-tile-header"})
                     latest_posted_job = None
                     for job in jobs_headers:
                         posted_time = job.text.split()
                         posted_time = posted_time[1] + " " + posted_time[2] + " " + posted_time[3]
                         if "day" in posted_time:
-                            print(f"Premium job{job.text} ")
+                            print(f"Premium job {job.text} ")
                         else:
                             latest_posted_job = job
                             break
@@ -65,7 +63,7 @@ def getJobs():
                     posted_time = posted_job_list[1] + " " + posted_job_list[2] + " " + posted_job_list[3]
                     print(f"Posted Time is : {posted_time}")
                     if "minute" in posted_time or "second" in posted_time:
-                        pb.push_note(f"New {JOB_TYPE_TO_CONSIDER[index]} Project", latest_posted_job.text)
+                        pb.push_note(f"New {JOB_TYPE_TO_CONSIDER[index]} Project", latest_posted_job.text+" job Link:  " + job_url)
                         print("Notification Sent")
                 except Exception as e:
                     print(f"Some Exception Occurred  {e}")
@@ -73,7 +71,7 @@ def getJobs():
                     print("WAIT_TIME_AFTER_SCRAPING")
                     time.sleep(WAIT_TIME_AFTER_SCRAPING)
             print("Sleeping for 5 minutes")
-            time.sleep(300)
+            time.sleep(WAIT_TIME_AFTER_SCRAPING_ALL)
         else:
             print("Wifi Not Connected")
 
